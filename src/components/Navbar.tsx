a0222d1b7a749e5989a8bc4949d54464a9dff5a7
AlvaroDiazTorres <90872843+AlvaroDiazTorres@users.noreply.github.com>
2025-06-14 20:33:35 +0200

    Update Navbar.tsx
    Quitar importación automática de React

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import type { UserProps } from '../models/User';
 import logo from '../assets/logos/logo_colores.png';
